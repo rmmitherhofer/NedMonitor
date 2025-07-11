1e34238f6a9eec1519bcdddc6ecd09682d36dbef
Renato Matos dos Santos Mitherhofer <renato.matos1@hotmail.com>
2025-07-11 12:30:56 -0300

    coletar dados BD

---
+++ src/NedMonitor.DataInterceptors/Configurations/NedMonitorDataInterceptorsConfigurations.cs
@@ -0,0 +1,32 @@
+﻿using Common.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NedMonitor.DataInterceptors.Interceptors;
+using NedMonitor.DataInterceptors.Services;
+
+namespace NedMonitor.DataInterceptors.Configurations;
+
+public static class NedMonitorDataInterceptorsConfigurations
+{
+    public static IServiceCollection AddNedMonitor(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services, nameof(IServiceCollection));
+
+        services.TryAddSingleton<EfQueryCounter>();
+        services.TryAddSingleton<DapperQueryCounter>();
+        services.TryAddSingleton<IQueryCounter, CompositeQueryCounter>();
+
+        return services;
+    }
+
+
+    public static IApplicationBuilder UseNedMonitorMiddleware(this IApplicationBuilder app)
+    {
+        ArgumentNullException.ThrowIfNull(app, nameof(IApplicationBuilder));
+
+        app.TryUseMiddleware<QueryCounterResetMiddleware>();
+
+        return app;
+    }
+}
---
+++ src/NedMonitor.DataInterceptors/Interceptors/DapperQueryCounter.cs
@@ -0,0 +1,21 @@
+﻿namespace NedMonitor.DataInterceptors.Interceptors;
+
+/// <summary>
+/// Contador de queries para Dapper, baseado em AsyncLocal para contexto de requisição.
+/// </summary>
+public class DapperQueryCounter : IQueryCounter
+{
+    private static readonly AsyncLocal<int> _count = new();
+
+    /// <inheritdoc/>
+    public void Reset() => _count.Value = 0;
+
+    /// <inheritdoc/>
+    public int GetCount() => _count.Value;
+
+    /// <summary>
+    /// Incrementa o contador de queries.
+    /// Deve ser chamado pelo wrapper da conexão Dapper.
+    /// </summary>
+    public void Increment() => _count.Value++;
+}
